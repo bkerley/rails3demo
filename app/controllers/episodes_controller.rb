8a97b50244476bdbd7e7265431d824b051a84d9b
Bryce Kerley <bkerley@brycekerley.net>
2010-09-14 15:12:21 -0400

    use respond_with

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,83 +1,42 @@
 class EpisodesController < ApplicationController
-  # GET /episodes
-  # GET /episodes.xml
+  respond_to :html, :xml, :json
+
   def index
     @episodes = Episode.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @episodes }
-    end
+    respond_with Episode.all
   end
 
-  # GET /episodes/1
-  # GET /episodes/1.xml
   def show
     @episode = Episode.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @episode }
-    end
+    respond_with @episode
   end
 
-  # GET /episodes/new
-  # GET /episodes/new.xml
   def new
     @episode = Episode.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @episode }
-    end
+    respond_with @episode
   end
 
-  # GET /episodes/1/edit
   def edit
     @episode = Episode.find(params[:id])
   end
 
-  # POST /episodes
-  # POST /episodes.xml
   def create
     @episode = Episode.new(params[:episode])
+    flash[:notice] = 'Episode was successfully created.' if @episode.save
 
-    respond_to do |format|
-      if @episode.save
-        format.html { redirect_to(@episode, :notice => 'Episode was successfully created.') }
-        format.xml  { render :xml => @episode, :status => :created, :location => @episode }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @episode.errors, :status => :unprocessable_entity }
-      end
-    end
+    respond_with @episode
   end
 
-  # PUT /episodes/1
-  # PUT /episodes/1.xml
   def update
     @episode = Episode.find(params[:id])
-
-    respond_to do |format|
-      if @episode.update_attributes(params[:episode])
-        format.html { redirect_to(@episode, :notice => 'Episode was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @episode.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Episode was successfully updated.' if @episode.save
+    respond_with @episode
   end
 
-  # DELETE /episodes/1
-  # DELETE /episodes/1.xml
   def destroy
     @episode = Episode.find(params[:id])
     @episode.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(episodes_url) }
-      format.xml  { head :ok }
-    end
+    respond_with @episode
   end
 end
